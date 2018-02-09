7148a664c0183a210819d85888956ef91d6a9af6
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2018-02-09 10:45:35 -0500

    Image is moving with control keys. Text is being displayed (not all)

--- MonoGameInputAndText/MonoGameInputAndText/Game1.cs
+++ MonoGameInputAndText/MonoGameInputAndText/Game1.cs
@@ -2,6 +2,16 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+/// <summary>
+/// IGME-106 - Game Development and Algorithmic Problem Solving
+/// Practice Exercise 7 - MonoGame Basics
+/// Class Description   : Main class and main menu for a number guessing game
+/// Author              : Benjamin Kleynhans
+/// Modified By         : Benjamin Kleynhans
+/// Date                : February 7, 2018
+/// Filename            : Game1.cs
+/// </summary>
+
 namespace MonoGameInputAndText
 {
     /// <summary>
@@ -12,10 +22,24 @@ namespace MonoGameInputAndText
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        private Texture2D beetleRightFacing;                // Create image (texture) variable
+        private Vector2 position;                           // Create position (vector) variable
+        private SpriteFont spriteFont;
+
+        private float XPos = 5;                             // Create X and Y position variables
+        private float YPos = 5;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            Window.Position = new Point(                    // Center the game view on the screen
+                (GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2) -
+                    (graphics.PreferredBackBufferWidth / 2),
+                (GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2) -
+                    (graphics.PreferredBackBufferHeight / 2)
+            );
         }
 
         /// <summary>
@@ -27,6 +51,7 @@ namespace MonoGameInputAndText
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            Vector2 vector2 = new Vector2(XPos, YPos);          // Set initial location for texture
 
             base.Initialize();
         }
@@ -39,8 +64,11 @@ namespace MonoGameInputAndText
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Load texture
+            beetleRightFacing = Content.Load<Texture2D>("BeatleRightFacing");
+            // Load sprite font
+            spriteFont = Content.Load<SpriteFont>("spriteFont");
 
-            // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
@@ -62,7 +90,11 @@ namespace MonoGameInputAndText
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            //XPos++;                                             // Increase X position of texture by 1
+
+            //position = new Vector2(XPos, YPos);                 // Update position variable
+
+            ProcessInput();
 
             base.Update(gameTime);
         }
@@ -73,11 +105,41 @@ namespace MonoGameInputAndText
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.MonoGameOrange);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();                                // Begin draw process
+
+            spriteBatch.Draw(beetleRightFacing, position, Color.White);
+            spriteBatch.DrawString(spriteFont, "test", position, Color.Blue);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void ProcessInput()
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.A))                    // Move left
+            {
+                position.X = position.X - 5;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.D))                    // Move right
+            {
+                position.X = position.X + 5;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.W))                    // Move up
+            {
+                position.Y = position.Y - 5;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.S))                    // Move down
+            {
+                position.Y = position.Y + 5;
+            }
         }
     }
 }
